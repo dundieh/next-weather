91637507d3bb6561b9cb02d92c1cc3c2cf2b0a4e
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-04 18:10:57 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import axios from 'axios';
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.NEXT_WEATHER_KEY}`;
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+  const [lloading, setLoading] = useState(false);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    axios.get(url).then((res) => {
+      setWeather(res.data);
+      console.log(res);
+    });
+
+    setCity('');
+  }
 
   return (
     <div>
@@ -9,6 +26,7 @@ export default function Home() {
         <title>Weather - Next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <button onClick={fetchWeather}>Search</button>
     </div>
   );
 };
