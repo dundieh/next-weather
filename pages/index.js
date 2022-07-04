7ca126ac0ddff3ecbb2793d80ea65e05e5bbb75c
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-04 19:12:22 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 import background from '../public/background.jpg';
 
 export default function Home() {
@@ -27,9 +28,17 @@ export default function Home() {
         <title>Weather - Next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Image src={background} alt='' layout='fill' className='object-cover' />
-      </main>
+      <div className='top-0 bottom-0 right-0 left-0 absolute z-[1] bg-black/40'>
+        <Image src={background} alt='background' layout='fill' className='object-cover' />
+      </div>
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
+        <form className='flex justify-between items-center w-full m-auto p-3 bg-transparent border text-white rounded-2xl'>
+          <input type='text' placeholder='search' className='bg-transparent border-none text-white focus:outline-none text-xl placeholder:text-white w-full' />
+          <button onClick={fetchWeather}>
+            <BsSearch size={20} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
