5becf4e9003c4ccdd8442825d53401d9fee20114
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-04 16:56:03 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 
 export default function Home() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.NEXT_WEATHER_KEY}`;
+
   return (
     <div>
       <Head>
