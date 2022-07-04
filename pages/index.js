281b746443a457cc1596ba9a35234a111d9c4aa2
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-04 16:57:15 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Weather - Next.js</title>
+        <title>Weather - Next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
