72798c3b7484e31a2100475e72f7c1898de4c503
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-04 16:36:03 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Weather</title>
+        <title>Weather - Next.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
