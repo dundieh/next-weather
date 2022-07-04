d1f8760136f227865c48d74edad72a4b6298b7d4
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-04 20:48:59 +0200

    init

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import spinner from '../public/spinner.gif';
 
 export default function Spinner() {
-    return (
-      <Image className='w-[200px] m-auto block' src={spinner} alt='loading' />
-    );
+  return (
+    <Image className='w-[200px] m-auto block' src={spinner} alt='loading' />
+  );
 }
--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 export default function Weather({ data }) {
     return (
-        <div>
-            <div>
-                <div>
-                    <Image src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} width={100} height={100} alt='case' />
+        <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-aut0 p-4 text-gray-300 z-10'>
+            <div className='relative flex justify-between pt-12'>
+                <div className='flex flex-col items-center'>
+                    <Image src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} width={100} height={100} alt='' />
+                    <p className='text-2xl'>{data.weather[0].main}</p>
                 </div>
+                <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
             </div>
+            <div></div>
         </div>
     );
 }
